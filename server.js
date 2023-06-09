7357c0ac494e0678dfc52cecb6f0d0f40c225a25
JosephHuy1906 <huydeptrai19062002@gmail.com>
2023-06-09 14:54:44 +0700

    add

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import bodyParser from 'body-parser';
 import connect from './configs/database.js';
 import rootRouter from './router/index.js';
+import cors from 'cors'
 import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import YAML from 'yaml';
@@ -12,6 +13,11 @@ const app = express();
 const port = process.env.PORT || 3002;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+    cors({
+        origin: '*'
+    })
+)
 app.use('/api', rootRouter);
 app.use('/images', express.static('public/images'))
 
